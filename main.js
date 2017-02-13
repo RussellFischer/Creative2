7a7f129176d4e69035af78c21a05dab518e1c175
Jordan EC2 <jnielson94@gmail.com>
2017-02-13 17:46:23 +0000

    Adding into html

--- main.js
+++ main.js
@@ -39,18 +39,21 @@ var foodNouns = [
 ];
 $(document).ready(function() {
     $('#img1').click(function() {
-  $('#img1').attr("src", "https://source.unsplash.com/category/building/400x300");
+  $('#img1').attr("src", "https://source.unsplash.com/category/buildings/400x300");
   descriptorString += buildingAdjectives[i++];
+  $("#stringGenerator").text = descriptorString;
       });
 
     $('#img2').click(function() {
   $('#img2').attr("src", "https://source.unsplash.com/category/nature/400x300");
   descriptorString += natureAdjectives[i++];
+  $("#stringGenerator").text = descriptorString;
       });
 
     $('#img3').click(function() {
   $('#img3').attr("src", "https://source.unsplash.com/category/food/400x300");
   descriptorString += foodAdjectives[i++];
+  $("#stringGenerator").text = descriptorString;
       });
 
     });
