7794e388dd7eaca07c0136409f83c8dcb9389ffe
RussellFischer <russellfischer@byu.edu>
2017-02-13 10:35:48 -0700

    splitting into files

---
+++ main.js
@@ -0,0 +1,11 @@
+function getThreeImages() {
+  $('#img1').attr("src", "https://source.unsplash.com/category/food/400x300");
+}
+
+$(document).ready(function() {
+    $('#img1').mouseover(function() {
+
+  $('#img1').attr("src", "https://source.unsplash.com/category/food/400x300");
+      });
+
+    });
